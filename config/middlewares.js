e25d9f7079a0bc9666d6deb858da6699ed97d880
charly <charlygame@126.com>
2020-08-08 22:19:40 +0800

    添加跨域中间件

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,10 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 // 处理中间件使用
 module.exports = function (app) {
     console.log('start middlewares');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(cors());
 }
